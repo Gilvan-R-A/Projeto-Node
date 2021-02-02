82b6ac93f3754e8bfb99fe7cdb7bd9884bd3e600
Gilvan Alves <gilvan.dev@gmail.com>
2021-02-02 16:43:33 -0300

    Classe que cria as especialidades dos profissionais que utilizam o sistema

---
+++ model/Especialidade.js
@@ -0,0 +1,7 @@
+module.exports = class Especialidade{
+    constructor(idEspecialidade, nomeEspecialidade, categoriaEspecialidade){
+        this.idEspecialidade = idEspecialidade;
+        this.nomeEspecialidade = nomeEspecialidade;
+        this.categoriaEspecialidade = categoriaEspecialidade;
+    }
+}
